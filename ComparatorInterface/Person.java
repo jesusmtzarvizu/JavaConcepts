9e73fe93e5ceb3b009cafaaebfe724b7eb21cc8b
jesusmtzarvizu <jarvizu@nearsoft.com>
2017-01-19 11:53:14 -0700

    Create Person.java

---
+++ ComparatorInterface/Person.java
@@ -0,0 +1,14 @@
+public class Person {
+
+    String name;
+    String lastName;
+    int age;
+
+     Person(String n,String lastName,int a){
+
+        this.name=n;
+        this.lastName=lastName;
+        this.age=a;
+
+    }
+}
