8dcf7164559b6e2464106655483e815d7ec75658
jesusmtzarvizu <jarvizu@nearsoft.com>
2017-01-20 11:37:31 -0700

    Update Factorial.java

--- Recursive/Factorial.java
+++ Recursive/Factorial.java
@@ -1,4 +1,6 @@
-int factorial(int n) {
+class Factorial(){
+        
+        int factorial(int n) {
         if (n < 0) {
             return -1;
         } else if (n == 0) {
@@ -7,3 +9,7 @@ int factorial(int n) {
             return n * factorial(n - 1);
         }
     }
+
+}
+        
+        
