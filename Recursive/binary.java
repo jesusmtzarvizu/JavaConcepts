2864673c468bdcfd432f7329dce2e133da81290a
jesusmtzarvizu <jarvizu@nearsoft.com>
2017-01-20 11:13:54 -0700

    A class that converts decimal to binary

---
+++ Recursive/binary.java
@@ -0,0 +1,14 @@
+public class binary {
+
+    void print_binary(int n){
+        if(n>=2){
+           print_binary(n/2);
+            System.out.print(n%2);
+        }
+        else{
+            System.out.print(n);
+        }
+
+
+    }
+}
